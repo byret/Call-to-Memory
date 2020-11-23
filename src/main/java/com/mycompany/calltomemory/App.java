54a8d7d2d4e58db52109c994b08658f96a6f5b09
byret <byret@gmail.com>
2020-11-23 04:17:55 +0100

    wyrównanie tekstu, kodowanie, nieco lepszy algorytm porównywania stringów

--- src/main/java/com/mycompany/calltomemory/App.java
+++ src/main/java/com/mycompany/calltomemory/App.java
@@ -1,25 +1,24 @@
 package com.mycompany.calltomemory;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.RandomAccessFile;
-import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -28,26 +27,27 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
-import org.apache.commons.lang3.StringUtils;
-
-
-
 /**
  * JavaFX App
  */
 public class App extends Application {
-
+   
     BorderPane borderPane = new BorderPane();
-    VBox textVBox = new VBox();
-    File file;
+    GridPane gridPane = new GridPane();
     int numOfWords;
-    int numOfRows;
-    int percentOfWords = 5;
+    int percentOfWords;
     int plus = 5;
     
     @Override
@@ -66,43 +66,23 @@ public class App extends Application {
     public VBox menuLoad(Stage stage){
         MenuBar menuBar = new MenuBar();
         Menu menu1 = new Menu();
-        Label l1 = new Label("File");
-        l1.setStyle("-fx-text-fill: white;");
-        menu1.setGraphic(l1);
+        Label t = new Label("File");
+        t.setStyle("-fx-text-fill: white;");
+        menu1.setGraphic(t);
         MenuItem menuItem1 = new MenuItem("New file...");
         MenuItem menuItem2 = new MenuItem("Open file...");
         MenuItem menuItem3 = new MenuItem("Save");
-        menu1.getItems().addAll(menuItem1, menuItem2, menuItem3);
+        MenuItem menuItem4 = new MenuItem("Save as...");
+        menu1.getItems().addAll(menuItem1, menuItem2, menuItem3, menuItem4);
         
         // TODO 
-        Menu menu2 = new Menu();
-        Label l2 = new Label("Edit");
-        l2.setStyle("-fx-text-fill: white;");
-        menu2.setGraphic(l2);
-        
-        Menu menu3 = new Menu();
-        Label l3 = new Label("Window");
-        l3.setStyle("-fx-text-fill: white;");
-        menu3.setGraphic(l3);
-        
-        Menu menu4 = new Menu();
-        Label l4 = new Label("Export");
-        l4.setStyle("-fx-text-fill: white;");
-        menu4.setGraphic(l4);
-        
-        Menu menu5 = new Menu();
-        Label l5 = new Label("Help");
-        l5.setStyle("-fx-text-fill: white;");
-        menu5.setGraphic(l5);
-        
+        Menu menu2 = new Menu("Edit");
+        Menu menu3 = new Menu("Window");
+        Menu menu4 = new Menu("Export");
+        Menu menu5 = new Menu("Help");
         menuBar.getMenus().addAll(menu1, menu2, menu3, menu4, menu5);
         VBox vBox = new VBox(menuBar);
         menuBar.setStyle("-fx-background-color : #819dff;");
-        
-        // New file clicked
-        menuItem1.setOnAction((ActionEvent e) -> {
-            
-        });
         
         // Open file clicked
         menuItem2.setOnAction((ActionEvent e) -> {
@@ -110,61 +90,29 @@ public class App extends Application {
             fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
             File selectedFile = fileChooser.showOpenDialog(stage);
             String fileName = "resources/" + selectedFile.getName();
-            file = new File(fileName);
+            File oFile = new File(fileName);
             try {
-                Files.copy(selectedFile.toPath(), file.toPath());
-                workWithText(stage);
+                Files.copy(selectedFile.toPath(), oFile.toPath());
+                workWithText(stage, oFile);
             } catch (IOException ex) {
                 // TODO WYJATEK - NIE UDALO SIE SKOPIOWAC
-                System.err.println("Problem reading file.");
-            }
-        });
-        
-        // Save clicked
-        menuItem3.setOnAction((ActionEvent e) -> {
-            if (file != null){
-             RandomAccessFile raf;
-                try {
-                    raf = new RandomAccessFile(file.getPath(), "rw");
-                    long length = raf.length() - 1;
-                    byte b;
-                    do {                     
-                        length -= 1;
-                        raf.seek(length);
-                        b = raf.readByte();
-                    } while(b != 10);
-                    raf.setLength(length+1);
-                    Files.writeString(file.toPath(), "endOfFile " + numOfWords + ' ' + percentOfWords, StandardOpenOption.APPEND);
-                    raf.close();
-                } catch (FileNotFoundException ex) {
-                    ex.printStackTrace();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }      
             }
         });
         
         return vBox;
     }
     
-    public void workWithText(Stage stage) throws FileNotFoundException, IOException{
-        Locale.setDefault(new Locale("ru"));
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
-        Scanner sc = new Scanner(bufferedReader);
+    public void workWithText(Stage stage, File file) throws FileNotFoundException, IOException{
+        Scanner sc = new Scanner(file);
         VBox vbox = new VBox();
         vbox.setAlignment(Pos.CENTER);
         Label top = new Label("Try to remember the text");
         top.setStyle("-fx-font-size: 30pt;");
-        textVBox.setAlignment(Pos.CENTER);
+        gridPane.setAlignment(Pos.CENTER);
         
         boolean isFirstTime = true;
         
         for (int i = 0; sc.hasNextLine(); i++) {
-            //int row = 0;
-            int word = 0;
-            HBox hBox = new HBox();
-            hBox.setAlignment(Pos.CENTER);
-            hBox.setSpacing(10);
             String line = sc.nextLine();
             if (line.contains("endOfFile")){
                 isFirstTime = false;
@@ -174,41 +122,33 @@ public class App extends Application {
             }
             else {
                 int wordLength = 0;
+                int word = 0;
                 for (int j = 0; j < line.length(); j++){                  
                     if (!Character.isWhitespace(line.charAt(j)) && j + 1 < line.length())
                         wordLength++;
                     else {
                         Label label = new Label(line.substring(j - wordLength, j));
-                        if (j + 1 == line.length())
-                            label = new Label(line.substring(j - wordLength, j + 1));
-                        System.setProperty("file.encoding","utf-8");
-                        System.out.println("aaaaaa " + System.getProperty("file.encoding"));
-                        label.setStyle("-fx-font-size: 15pt; -fx-font-family: \"Arial\";");
-                      
-                        hBox.getChildren().add(label);
+                        label.setStyle("-fx-font-size: 15pt;");
+                        gridPane.add(label, word, i);
                         wordLength = 0;
                         word++;
                     }
-                }          
-            }               
-            textVBox.getChildren().add(hBox);
-            numOfRows++;
-            if (isFirstTime){
+                }
+                if (isFirstTime)              
                     numOfWords += word;
-                }    
+            }    
         }
         if (isFirstTime){
             Files.writeString(file.toPath(), "\nendOfFile " + numOfWords + ' ' + percentOfWords, StandardOpenOption.APPEND);
         }
-        Label progress = new Label();
-        progress.setStyle("-fx-font-size: 20pt; -fx-background-color : #819dff; -fx-text-fill : white;");
         Button buttonOk = new Button("Ok!");
         vbox.setSpacing(30);
-        vbox.getChildren().addAll(top, textVBox, progress, buttonOk);
+        vbox.getChildren().addAll(top, gridPane, buttonOk);
         
         buttonOk.setOnAction((ActionEvent e) -> {
             top.setText("Complete the text");
-            removeWords(stage, vbox);
+            percentOfWords += 5; 
+            removeWords(stage, vbox, numOfWords, percentOfWords);
         });
         
         borderPane.setTop(menuLoad(stage));
@@ -218,68 +158,37 @@ public class App extends Application {
         stage.show();
     }
     
-
-    
-    public void removeWords(Stage stage, VBox vbox){ 
-        final boolean isLastTime;
+    public void removeWords(Stage stage, VBox vbox, final int numOfWords, final int percentOfWords){ 
+        
         Button buttonCheck = new Button("Check");
-        vbox.getChildren().remove(3);
+        vbox.getChildren().remove(2);
         vbox.getChildren().add(buttonCheck);
         Random rand = new Random();
         int numOfWordsToRemove = numOfWords * percentOfWords / 100;
-        if (percentOfWords >= 100){
-            numOfWordsToRemove = numOfWords;
-            isLastTime = true;
-        }
-        else isLastTime = false;
-        
-        ((Label)vbox.getChildren().get(2)).setText(String.valueOf(numOfWordsToRemove) + '/' + String.valueOf(numOfWords));
-        
         MyNode [] nodes = new MyNode[numOfWordsToRemove];
-        List<MyPoint> list = new ArrayList<>();
-        
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < numOfWordsToRemove; i++){
             nodes[i] = new MyNode();
-            int indexRow; // X
-            int index;    // Y
-            MyPoint point = new MyPoint(0, 0);
-            while (true){ 
-                indexRow = rand.nextInt(numOfRows);
-                int numOfWordsInPerLine =((HBox)(textVBox.getChildren().get(indexRow))).getChildren().size();
-                if (numOfWordsInPerLine == 0)
-                    continue;
-                index = rand.nextInt(numOfWordsInPerLine);
-                point.setX(indexRow); 
-                point.setY(index);
-                if (!list.contains(point))
-                    break;
-            }
-            list.add(point);
-            if (textVBox.getChildren().get(indexRow) instanceof HBox){
-                if (((HBox)(textVBox.getChildren().get(indexRow))).getChildren().get(index) instanceof Label){
-                //nodes[i].setIndex(index);
-                nodes[i].setIndexX(indexRow);
-                nodes[i].setIndexY(index);
-                nodes[i].setString(((Label)((HBox)(textVBox.getChildren().get(indexRow))).getChildren().get(index)).getText());
+            int index;
+            do 
+                index = rand.nextInt(numOfWords);
+            while (list.contains(index));
+            list.add(index);
+            if (gridPane.getChildren().get(index) instanceof Label){
+                nodes[i].setIndex(index);
+                nodes[i].setIndexX(gridPane.getColumnIndex(gridPane.getChildren().get(index)));
+                nodes[i].setIndexY(gridPane.getRowIndex(gridPane.getChildren().get(index)));
+                nodes[i].setString(((Label)gridPane.getChildren().get(index)).getText());
                 nodes[i].writeAll();
-                ((HBox)(textVBox.getChildren().get(indexRow))).getChildren().remove(index);
+                gridPane.getChildren().remove(index);
                 TextField textField = new TextField ();
-                textField.setPrefWidth(60);
-                textField.textProperty().addListener((ov, prevText, currText) -> {
-                    Platform.runLater(() -> {
-                    Text text = new Text(currText);
-                    text.setFont(textField.getFont());
-                    double width = text.getLayoutBounds().getWidth() + textField.getPadding().getLeft() + textField.getPadding().getRight() + 2d;
-                    textField.setPrefWidth(width); 
-                    });
-                });
-                ((HBox)(textVBox.getChildren().get(indexRow))).getChildren().add(index, textField);
-                }
+                textField.setPrefWidth(25);
+                gridPane.add(textField, nodes[i].getIndexX(), nodes[i].getIndexY());
             }
         }
        
         buttonCheck.setOnAction((ActionEvent e) -> {
-            checkWords(stage, vbox, nodes, isLastTime);
+            checkWords(stage, vbox, nodes, numOfWords);
         });
         
         Scene scene = new Scene(borderPane);
@@ -287,17 +196,17 @@ public class App extends Application {
         stage.show();    
     }
     
-    public void checkWords(Stage stage, VBox vbox, MyNode [] nodes, boolean isLastTime){ 
+    public void checkWords(Stage stage, VBox vbox, MyNode [] nodes, final int numOfWords){ 
         int numOfRight = 0, numOfWrong = 0;
         for (MyNode node : nodes){
-            if (((HBox)(textVBox.getChildren().get(node.getIndexX()))).getChildren().get(node.getIndexY()) instanceof TextField){
-                if (compareTwoStrings(node.getString(), ((TextField)((HBox)(textVBox.getChildren().get(node.getIndexX()))).getChildren().get(node.getIndexY())).getText())){
+            if (getNodeByRowColumnIndex(node.getIndexY(), node.getIndexX(), gridPane) instanceof TextField){
+                if ((((TextField)getNodeByRowColumnIndex(node.getIndexY(), node.getIndexX(), gridPane)).getText()).equalsIgnoreCase(node.getString())){
                     numOfRight++;
-                    ((TextField)((HBox)(textVBox.getChildren().get(node.getIndexX()))).getChildren().get(node.getIndexY())).setStyle("-fx-control-inner-background: #cafeca;");                  
+                    ((TextField)getNodeByRowColumnIndex(node.getIndexY(), node.getIndexX(), gridPane)).setStyle("-fx-control-inner-background: #cafeca;");                  
                 }
                 else{
                     numOfWrong++;
-                    ((TextField)((HBox)(textVBox.getChildren().get(node.getIndexX()))).getChildren().get(node.getIndexY())).setStyle("-fx-control-inner-background: #ffcbcb;");                    
+                    ((TextField)getNodeByRowColumnIndex(node.getIndexY(), node.getIndexX(), gridPane)).setStyle("-fx-control-inner-background: #ffcbcb;");                    
                 }
             }
         }
@@ -310,40 +219,37 @@ public class App extends Application {
             plus -= numOfWrong;
             if (plus < 0) plus = 0;
         }
-        percentOfWords += plus;
         
         Button buttonOk = new Button("Ok");
-        vbox.getChildren().remove(3);
+        vbox.getChildren().remove(2);
         vbox.getChildren().add(buttonOk);
         buttonOk.setOnAction((ActionEvent e) -> {
             ((Label)vbox.getChildren().get(0)).setText("Complete the text");
             for (MyNode node : nodes){
-                if (((HBox)(textVBox.getChildren().get(node.getIndexX()))).getChildren().get(node.getIndexY()) instanceof TextField){
-                    ((HBox)(textVBox.getChildren().get(node.getIndexX()))).getChildren().remove(node.getIndexY());
+                if (getNodeByRowColumnIndex(node.getIndexY(), node.getIndexX(), gridPane) instanceof TextField){
+                    gridPane.getChildren().remove(getNodeByRowColumnIndex(node.getIndexY(), node.getIndexX(), gridPane));    
                     Label label = new Label(node.getString());
                     label.setStyle("-fx-font-size: 15pt;");
-                    ((HBox)(textVBox.getChildren().get(node.getIndexX()))).getChildren().add(node.getIndexY(), label);
+                    gridPane.add(label, node.getIndexX(), node.getIndexY());
                 }
             }
-            removeWords(stage, vbox);
+            removeWords(stage, vbox, numOfWords, percentOfWords + plus);
         });
     }
     
     // TODO - POROWNYWANIE SLOW
-    public boolean compareTwoStrings(String firstString, String secondString){
-        firstString = firstString.toLowerCase();
-        secondString = secondString.toLowerCase();
-        firstString = removeNonAlphanumerics(firstString);
-        secondString = removeNonAlphanumerics(secondString);
-        int i = StringUtils.getLevenshteinDistance(firstString, secondString);
-        return i <= firstString.length()/4;
-    }
     
-    public String removeNonAlphanumerics (String string){
-        String[] stringArray = string.split("\\W+");
-        String result = new String();
-        for (int i = 0; i < stringArray.length; i++)
-            result += stringArray[i];
+    public Node getNodeByRowColumnIndex (final int row, final int column, GridPane gridPane) {
+        Node result = null;
+        ObservableList<Node> childrens = gridPane.getChildren();
+
+        for (Node node : childrens) {
+            if(gridPane.getRowIndex(node) == row && gridPane.getColumnIndex(node) == column) {
+                result = node;
+                break;
+            }
+        }
+
         return result;
     }
      
